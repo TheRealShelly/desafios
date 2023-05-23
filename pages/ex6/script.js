896d53cb375acdcdb52e9ebd7b6e4fe3209a82cb
TheRealShelly <68953479+TheRealShelly@users.noreply.github.com>
2023-05-23 08:06:09 -0300

    Update script.js

--- pages/ex6/script.js
+++ pages/ex6/script.js
@@ -3,8 +3,9 @@ let msg = document.getElementById('msg')
 
 bt.addEventListener('click', (event) => {
     event.preventDefault()
+    let c = Number(prompt('qual a cotação atual do dolar?'))
     let d = Number(prompt('Quantos R$ você tem na carteira?'))
 
-    msg.innerHTML = `Você consegue comprar ${d / 5} dolares! <br> 1 dolar = 5 reais`
+    msg.innerHTML = `Você consegue comprar ${d / c} dolares!`
 
 })
